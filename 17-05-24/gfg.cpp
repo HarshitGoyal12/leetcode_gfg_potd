1909058b85e57c4b383d62b36763c13c37f7e5fd
Harshit Goyal <harshgoyal1357@gmail.com>
2024-05-17 10:23:28 +0530

    updating

--- 17-05-24/gfg.cpp
+++ 17-05-24/gfg.cpp
@@ -4,16 +4,28 @@ using namespace std;
 class Solution {
   public:
     int findPair(int n, int x, vector<int> &arr) {
-        code here
-         sort(arr.begin(),arr.end());
-        int l = 0,r=1;
-        while(r < n){
-            int diff = arr[r] - arr[l];
-            if (diff == x && l != r) return 1; // Ensure l != r
-            else if (diff < x) r++;
-            else l++;
-            if (l == r) r++;
-        }
+        // code here
+        //  sort(arr.begin(),arr.end());
+        // int l = 0,r=1;
+        // while(r < n){
+        //     int diff = arr[r] - arr[l];
+        //     if (diff == x && l != r) return 1; // Ensure l != r
+        //     else if (diff < x) r++;
+        //     else l++;
+        //     if (l == r) r++;
+        // }
+        // return -1;
+
+        //method 2
+         unordered_set<int>s(arr.begin(),arr.end());
+        if(x==0 )
+            if(s.size()==arr.size())
+                return -1;
+            else
+                return 1;
+        for(int i=0;i<n;i++)
+            if(s.find(x+arr[i])!=s.end())
+                return 1;
         return -1;
     }
 };
