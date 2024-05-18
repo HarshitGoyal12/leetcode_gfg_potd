45732a74d7b9b58869a851e4dfcbaba0d9681397
Harshit Goyal <harshgoyal1357@gmail.com>
2024-05-18 12:49:15 +0530

    updating

--- 18-05-24/leetcode.cpp
+++ 18-05-24/leetcode.cpp
@@ -11,3 +11,30 @@ using namespace std;
       TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
   };
 
+class Solution {
+public:
+void answer(TreeNode*parent,TreeNode* child,int &cnt){
+    if(!child){
+        return ;
+    }
+    answer(child,child->left,cnt);
+    answer(child,child->right,cnt);
+    if(child->val>1){
+        int extraCoins=child->val-1;
+        child->val=1;
+        parent->val+=extraCoins;
+        cnt+=extraCoins;
+    }
+    else if(child->val<1){
+  int needed=1+abs(child->val);
+  child->val=1;
+  parent->val-=needed;
+  cnt+=needed;
+    }
+}
+ int distributeCoins(TreeNode* root) {
+        int cnt=0;
+        answer(root,root,cnt);
+        return cnt;
+    }
+};
