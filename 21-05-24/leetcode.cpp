de689d8cf4fd694f081d8af1e5da1ff5e2bebf38
Harshit Goyal <harshgoyal1357@gmail.com>
2024-05-22 18:45:25 +0530

    updating

--- 21-05-24/leetcode.cpp
+++ 21-05-24/leetcode.cpp
@@ -1,3 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Solution {
+    private:
+void solve(vector<int> nums,int index,vector<int>output,vector<vector<int>> &ans)
+{
+    if(index>=nums.size()){
+        ans.push_back(output);
+        return ;
+    }
+    output.push_back(nums[index]);
+    solve(nums,index+1,output,ans);
+    output.pop_back();
+    solve(nums,index+1,output,ans);
+
+}
+
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> output;
+         int index=0;
+        solve(nums,index,output,ans);
+        return ans;
+
+    }
+};
