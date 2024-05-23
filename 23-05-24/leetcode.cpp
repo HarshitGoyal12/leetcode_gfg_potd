92b6d3cd0ef7eed5ec9035eb40f8118a2de4b5d5
Harshit Goyal <harshgoyal1357@gmail.com>
2024-05-23 19:46:28 +0530

    updating

--- 23-05-24/leetcode.cpp
+++ 23-05-24/leetcode.cpp
@@ -2,3 +2,36 @@
 using namespace std;
 
 // 2597. The Number of Beautiful Subsets
+class Solution {
+public:
+    int beautifulSubsets(vector<int>& nums, int k) {
+        count = 0;
+        explore(nums, k, 0);
+        return count - 1;
+    }
+
+private:
+    int count;
+    unordered_map<int, int> visited;
+
+    void explore(vector<int>& nums, int k, int index) {
+        if (index == nums.size()) {
+            count++;
+            return;
+        }
+
+        int num = nums[index];
+        //not found
+        if (visited.find(num - k) == visited.end() &&
+            visited.find(num + k) == visited.end()) {
+            visited[num]++;
+            explore(nums, k, index + 1);
+            visited[num]--;
+            if (visited[num] == 0) {
+                visited.erase(num);
+            }
+        }
+        //found
+        explore(nums, k, index + 1);
+    }
+};
