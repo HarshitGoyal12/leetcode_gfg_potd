faec6119560917ae32abe90f63188e229e9196c9
Harshit Goyal <harshgoyal1357@gmail.com>
2024-06-01 13:13:51 +0530

    updating

--- 31-05-24/gfg.cpp
+++ 31-05-24/gfg.cpp
@@ -1,2 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+class Solution {
+  public:
+    int swapNibbles(int n) {
+        // code here
+         int fn=n>>4;
+
+        int sn=n&15;
+
+        return (sn<<4) | fn;
+    }
+};
